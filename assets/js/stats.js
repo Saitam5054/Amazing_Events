a3c4ddbcbfef4d7374433f1618925f6ef57fa05f
Saitam5054 <masosa361@hotmail.com>
2023-03-24 11:40:13 -0300

    update stats data base

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,276 +1,122 @@
-var data = {
-  "currentDate": "2022-01-01",
-  "events": [
-    {
-      _id: 1,
-      "image": "https://i.postimg.cc/Fs03hQDt/Collectivities-Party.jpg",
-      "name": "Collectivities Party",
-      "date": "2021-12-12",
-      "description": "Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
-      "category": "Food Fair",
-      "place": "Room A",
-      "capacity": 45000,
-      "assistance": 42756,
-      "price": 5
-    },
-    {
-      _id: 2,
-      "image": "https://i.postimg.cc/ZmD3Xf57/Korean-style.jpg",
-      "name": "Korean style",
-      "date": "2022-08-12",
-      "description": "Enjoy the best Korean dishes, with international chefs and awesome events.",
-      "category": "Food Fair",
-      "place": "Room A",
-      "capacity": 45000,
-      "assistance": 42756,
-      "price": 10
-    },
-    {
-      _id: 3,
-      "image": "https://i.postimg.cc/GmHRkbNV/Jurassic-Park.jpg",
-      "name": "Jurassic Park",
-      "date": "2021-11-02",
-      "description": "Let's go meet the biggest dinosaurs in the paleontology museum.",
-      "category": "Museum",
-      "place": "Field",
-      "capacity": 82000,
-      "assistance": 65892,
-      "price": 15
-    },
-    {
-      _id: 4,
-      "image": "https://i.postimg.cc/c4C2zXm8/Parisian-Museum.jpg",
-      "name": "Parisian Museum",
-      "date": "2022-11-02",
-      "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-      "category": "Museum",
-      "place": "Paris",
-      "capacity": 8200,
-      "estimate": 8200,
-      "price": 3500
-    },
-    {
-      _id: 5,
-      "image": "https://i.postimg.cc/KYD0jMf2/comicon.jpg",
-      "name": "Comicon",
-      "date": "2021-02-12",
-      "description": "For comic lovers, all your favourite characters gathered in one place.",
-      "category": "Costume Party",
-      "place": "Room C",
-      "capacity": 120000,
-      "assistance": 110000,
-      "price": 54
-    },
-    {
-      _id: 6,
-      "image": "https://i.postimg.cc/RZ9fH4Pr/halloween.jpg",
-      "name": "Halloween Night",
-      "date": "2022-02-12",
-      "description": "Come with your scariest costume and win incredible prizes.",
-      "category": "Costume Party",
-      "place": "Room C",
-      "capacity": 12000,
-      "estimate": 9000,
-      "price": 12
-    },
-    {
-      _id: 7,
-      "image": "https://i.postimg.cc/PrMJ0ZMc/Metallica-in-concert.jpg",
-      "name": "Metallica in concert",
-      "date": "2022-01-22",
-      "description": "The only concert of the most emblematic band in the world.",
-      "category": "Music Concert",
-      "place": "Room A"
-      , "capacity": 138000,
-      "estimate": 138000,
-      "price": 150
-    },
-    {
-      _id: 8,
-      "image": "https://i.postimg.cc/KvsSK8cj/Electronic-Fest.jpg",
-      "name": "Electronic Fest",
-      "date": "2021-01-22",
-      "description": "The best national and international DJs gathered in one place.",
-      "category": "Music Concert",
-      "place": "Room A",
-      "capacity": 138000,
-      "assistance": 110300,
-      "price": 250
-    },
-    {
-      _id: 9,
-      "image": "https://i.postimg.cc/fyLqZY9K/10-K-for-life.jpg",
-      "name": "10K for life",
-      "date": "2021-03-01",
-      "description": "Come and exercise, improve your health and lifestyle.",
-      "category": "Race",
-      "place": "Soccer field",
-      "capacity": 30000,
-      "assistance": 25698,
-      "price": 3
-    },
-    {
-      _id: 10,
-      "image": "https://i.postimg.cc/zv67r65z/15kny.jpg",
-      "name": "15K NY",
-      "date": "2022-03-01",
-      "description": "We'll be raising funds for hospitals and medical care in this unique event held in The Big Apple.",
-      "category": "Race",
-      "place": "New York",
-      "capacity": 3000000,
-      "assistance": 2569800,
-      "price": 3
-    },
-    {
-      _id: 11,
-      "image": "https://i.postimg.cc/Sst763n6/book1.jpg",
-      "name": "School's book fair",
-      "date": "2022-10-15",
-      "description": "Bring your unused school book and take the one you need.",
-      "category": "Book Exchange",
-      "place": "Room D1",
-      "capacity": 150000,
-      "estimate": 123286,
-      "price": 1
-    },
-    {
-      _id: 12,
-      "image": "https://i.postimg.cc/05FhxHVK/book4.jpg",
-      "name": "Just for your kitchen",
-      "date": "2021-11-09",
-      "description": "If you're a gastronomy lover come get the cookbook that best suits your taste and your family's.",
-      "category": "Book Exchange",
-      "place": "Room D6",
-      "capacity": 130000,
-      "assistance": 90000,
-      "price": 100
-    },
-    {
-      _id: 13,
-      "image": "https://i.postimg.cc/vH52y81C/cinema4.jpg",
-      "name": "Batman",
-      "date": "2021-03-11",
-      "description": "Come see Batman fight crime in Gotham City.",
-      "category": "Cinema",
-      "place": "Room D1",
-      "capacity": 11000,
-      "assistance": 9300,
-      "price": 225
-    },
-    {
-      _id: 14,
-      "image": "https://i.postimg.cc/T3C92KTN/scale.jpg",
-      "name": "Avengers",
-      "date": "2022-10-15",
-      "description": "Marvel's Avengers Premier in 3d, the start of an epic saga with your favourite superheroes.",
-      "category": "Cinema",
-      "place": "Room D1",
-      "capacity": 9000,
-      "estimate": 9000,
-      "price": 250
-    }
-  ]
-};
+fetch('https://mindhub-xj03.onrender.com/api/amazing')
+  .then(response => response.json())
+  .then(data => {
+    const maxAttendanceEvent = getMaxAttendanceEvent(data.events);
+    console.log(`Evento con mayor porcentaje de asistencia: ${maxAttendanceEvent.name}`);
 
-const { events } = data;
+    const minAttendanceEvent = getMinAttendanceEvent(data.events);
+    console.log(`Evento con menor porcentaje de asistencia: ${minAttendanceEvent.name}`);
 
-const maxAttendanceEvent = events.reduce((max, event) => {
-  const attendancePercent = (event.assistance / event.capacity) * 100;
-  return attendancePercent > max.attendancePercent ? { name: event.name, attendancePercent } : max;
-}, { name: '', attendancePercent: 0 });
+    const maxCapacityEvent = getMaxCapacityEvent(data.events);
+    console.log(`Evento con mayor capacidad: ${maxCapacityEvent.name}`);
 
+    const currentDate = new Date(data.currentDate);
+
+    const pastEvents = data.events.filter(event => new Date(event.date) < currentDate);
+    const futureEvents = data.events.filter(event => new Date(event.date) >= currentDate);
+
+    const pastCategories = calculateCategories(pastEvents);
+    const futureCategories = calculateCategories(futureEvents);
+
+    createRowTable(".tr1", { maxAttendanceEvent, minAttendanceEvent, maxCapacityEvent });
+    createRowTable(".tr2", pastCategories);
+    createRowTable(".tr3", futureCategories);
+  })
+  .catch(error => console.log(error));
+
+function getMaxAttendanceEvent(events) {
+  return events.reduce((max, event) => {
+    const attendancePercent = (event.assistance / event.capacity) * 100;
+    return attendancePercent > max.attendancePercent ? { name: event.name, attendancePercent } : max;
+  }, { name: '', attendancePercent: 0 });
+}
+
+function getMinAttendanceEvent(events) {
+  return events.reduce((min, event) => {
+    const attendancePercent = (event.assistance / event.capacity) * 100;
+    return attendancePercent < min.attendancePercent ? { name: event.name, attendancePercent } : min;
+  }, { name: '', attendancePercent: 100 });
+}
+
+function getMaxCapacityEvent(events) {
+  return events.reduce((max, event) => {
+    return event.capacity > max.capacity ? { name: event.name, capacity: event.capacity } : max;
+  }, { name: '', capacity: 0 });
+}
+
+const maxAttendanceEvent = getMaxAttendanceEvent(data.events);
 console.log(`Evento con mayor porcentaje de asistencia: ${maxAttendanceEvent.name}`);
 
-const minAttendanceEvent = events.reduce((min, event) => {
-  const attendancePercent = (event.assistance / event.capacity) * 100;
-  return attendancePercent < min.attendancePercent ? { name: event.name, attendancePercent } : min;
-}, { name: '', attendancePercent: 100 });
-
+const minAttendanceEvent = getMinAttendanceEvent(data.events);
 console.log(`Evento con menor porcentaje de asistencia: ${minAttendanceEvent.name}`);
 
-const maxCapacityEvent = events.reduce((max, event) => {
-  return event.capacity > max.capacity ? { name: event.name, capacity: event.capacity } : max;
-}, { name: '', capacity: 0 });
-
+const maxCapacityEvent = getMaxCapacityEvent(data.events);
 console.log(`Evento con mayor capacidad: ${maxCapacityEvent.name}`);
-
-tr1 = document.querySelector(".tr1");
-
-tr1.innerHTML = `<td>${maxAttendanceEvent.name} (${maxAttendanceEvent.attendancePercent.toFixed(2)}%)</td>
-                    <td>${minAttendanceEvent.name} (${minAttendanceEvent.attendancePercent.toFixed(2)}%)</td>
-                    <td>${maxCapacityEvent.name} (${maxCapacityEvent.capacity})</td>`;
-
-
 
 const currentDate = new Date(data.currentDate);
 
 const pastEvents = data.events.filter(event => new Date(event.date) < currentDate);
 const futureEvents = data.events.filter(event => new Date(event.date) >= currentDate);
 
-const pastCategories = {};
-pastEvents.forEach(event => {
-  const category = event.category;
-  const income = event.price * Math.min(event.assistance || event.estimate, event.capacity);
-  const attendance = (event.assistance || event.estimate) / event.capacity * 100;
+function calculateCategories(events) {
+  const categories = {};
+  events.forEach(event => {
+    const category = event.category;
+    const income = event.price * Math.min(event.assistance || event.estimate, event.capacity);
+    const attendance = (event.assistance || event.estimate) / event.capacity * 100;
 
-  if (pastCategories[category]) {
-    pastCategories[category].income += income;
-    pastCategories[category].attendance += attendance;
-    pastCategories[category].count++;
-  } else {
-    pastCategories[category] = {
-      income: income,
-      attendance: attendance,
-      count: 1
-    };
-  }
-});
-
-const futureCategories = {};
-futureEvents.forEach(event => {
-  const category = event.category;
-  const income = event.price * Math.min(event.estimate, event.capacity);
-  const attendance = event.estimate / event.capacity * 100;
-
-  if (futureCategories[category]) {
-    futureCategories[category].income += income;
-    futureCategories[category].attendance += attendance;
-    futureCategories[category].count++;
-  } else {
-    futureCategories[category] = {
-      income: income,
-      attendance: attendance,
-      count: 1
-    };
-  }
-});
-
-let tr2 = document.querySelector(".tr2");
-for (const category in pastCategories) {
-  const totalIncome = pastCategories[category].income;
-  const avgAttendance = pastCategories[category].attendance / pastCategories[category].count;
-
-  tr2.innerHTML += `<tr>
-                      <td>${category}</td>
-                      <td>$${totalIncome.toFixed(2)}</td>
-                      <td>${avgAttendance.toFixed(2)}%</td>
-                    </tr>`
+    if (categories[category]) {
+      categories[category].income += income;
+      categories[category].attendance += attendance;
+      categories[category].count++;
+    } else {
+      categories[category] = {
+        income: income,
+        attendance: attendance,
+        count: 1
+      };
+    }
+  });
+  return categories;
 }
 
-let tr3 = document.querySelector(".tr3");
-for (const category in futureCategories) {
-  const totalIncome = futureCategories[category].income;
-  const avgAttendance = futureCategories[category].attendance / futureCategories[category].count;
+const pastCategories = calculateCategories(pastEvents);
+const futureCategories = calculateCategories(futureEvents);
 
-  tr3.innerHTML += `<tr>
-                      <td>${category}</td>
-                      <td>$${totalIncome.toFixed(2)}</td>
-                      <td>${avgAttendance.toFixed(2)}%</td>
-                    </tr>`
+function createRowTable(selector, data) {
+  const table = document.querySelector(selector);
+
+  if (selector === ".tr1") {
+    // Formato de datos para la tabla tr1
+    table.innerHTML += `<td>${data.maxAttendanceEvent.name} (${data.maxAttendanceEvent.attendancePercent.toFixed(2)}%)</td>
+                        <td>${data.minAttendanceEvent.name} (${data.minAttendanceEvent.attendancePercent.toFixed(2)}%)</td>
+                        <td>${data.maxCapacityEvent.name} (${data.maxCapacityEvent.capacity})</td>`;
+  } else {
+    // Formato de datos para las tablas tr2 y tr3
+    for (const category in data) {
+      const totalIncome = data[category].income;
+      const avgAttendance = data[category].attendance / data[category].count;
+
+      table.innerHTML += `<tr>
+                            <td>${category}</td>
+                            <td>$${totalIncome.toFixed(2)}</td>
+                            <td>${avgAttendance.toFixed(2)}%</td>
+                          </tr>`;
+    }
+  }
 }
 
+const tr1Data = [
+  { name: maxAttendanceEvent.name, value: maxAttendanceEvent.attendancePercent, unit: "%" },
+  { name: minAttendanceEvent.name, value: minAttendanceEvent.attendancePercent, unit: "%" },
+  { name: maxCapacityEvent.name, value: maxCapacityEvent.capacity, unit: "" }
+];
 
+createRowTable(".tr1", { maxAttendanceEvent, minAttendanceEvent, maxCapacityEvent });
 
+createRowTable(".tr2", pastCategories);
+
+createRowTable(".tr3", futureCategories);
 
 /* const currentDate = new Date(data.currentDate);
 
